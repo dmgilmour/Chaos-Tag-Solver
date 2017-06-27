b45574924acfdf688870d4602d079e1e18e127ff
dmgilmour <dmgilmour@pitt.edu>
2017-06-26 20:36:49 -0400

    Everything

---
+++ ChaosSolver.py
@@ -0,0 +1,71 @@
+import sys
+import random
+from random import randint
+
+num_args = len(sys.argv)
+
+if num_args < 3 or num_args > 4:
+	print("Invalid number of arguments")
+	print(">ChaosSolver.py <simulations> <players>")
+	print("or")
+	print(">ChaosSolver.py <simulations> <lower-player-range> <upper-player-range>")
+	exit(1)
+
+num_runs = int(sys.argv[1])
+lower_range = int(sys.argv[2])
+if num_args == 3:
+	upper_range = lower_range + 1
+else:
+	upper_range = int(sys.argv[3])
+
+print("")
+
+for num_players in range(lower_range, upper_range):
+	runList = []
+	for i in range(num_runs):
+
+		# Players are stored by int alone, first line
+		# makes the player list
+		activePlayers = [i for i in range(num_players)]
+
+		# Make an empty array for each player which will
+		# later store the players they've captured
+		playerCaptures = [[] for i in range(num_players)]
+
+		tag_count = 0
+		while len(activePlayers) > 1:
+
+			# Get a random player
+			tagged_player = activePlayers[randint(0, len(activePlayers) - 1)]
+
+			# Remove the player from active players
+			activePlayers.remove(tagged_player)
+
+			# Randomly select a tagging player
+			tagging_player = random.choice(activePlayers)
+
+			# Add the tagged player to the tagging players
+			# capture list
+			playerCaptures[tagging_player].append(tagged_player)
+			"""
+			print("")
+			print(activePlayers)
+			print(tagged_player)
+			print(tagging_player)
+			print(playerCaptures)
+			"""
+			tag_count += 1
+
+			# Return all players captured by the tagged player
+			activePlayers += playerCaptures[tagged_player]
+
+			# Update the tagged players capture list to empty
+			playerCaptures[tagged_player] = [] 
+
+		runList.append(tag_count)
+
+	sum = 0
+	for i in runList:
+		sum += i	
+
+	print(num_players, sum / num_runs)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Chaos-Tag-Solver
+
+## Problem:
+
+There are N players of Chaos Tag. Whenever a player is tagged, they become inactive and can no longer play. A player becomes active again when the player who tagged them is tagged. The game ends when there is only one player remaining. Find the pattern for average number of tags needed to complete and number of players. (Note: if there are two players left and one is tagged, the players rendered inactive by the last tagged player become active before the game ends)
+
+## Solution
+
+The pattern is Number of tags = (2^(N - 1)) - 1
+
+Eg:
+
+2 : 1
+3 : 3
+4 : 7
+5 : 15
+6 : 31
+7 : 63
+...
